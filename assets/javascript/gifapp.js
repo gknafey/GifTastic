4961208efb4c01551d9e6c3cb5ec69be4871bca0
Garpken <gknafey@gmail.com>
2019-07-09 17:39:58 -0400

    messed with the gifapp.js to try and get the javascript to function correctly

--- assets/javascript/gifapp.js
+++ assets/javascript/gifapp.js
@@ -1,25 +1,24 @@
 
 
-var topics = ["Dog"]
+var topics = ["Dog", "Cat", "Armadillo"]
 makeButtons();
 
 function makeButtons() {
 
-    $("#buttons").empty();
+    // $("#buttons").empty();
 
-    for (var i=0; i < topics.length; i++) {
+    for (var i = 0; i < topics.length; i++) {
 
-        var a = $("<buttons id='dis' class='btn btn-info'>");
+        var a = $("<button id='dis' class='btn btn-info dis'>");
 
         a.addClass("animal");
 
-        a.attr("data-name", topics[i]);
+        a.attr("dataname", topics[i]);
 
         a.text(topics[i]);
 
         $("#buttons").append(a).append(" ");
 
-        console.log(topics);
     }
 }
 
@@ -35,45 +34,11 @@ $("#input").on("click", function(event) {
     
 });
 
-$("#buttons").on("click", buildQueryUrl);
+$(".dis").on("click", buildQueryUrl);
+
+function buildQueryUrl() {
     
-//     var animal = $(this).attr("data-name");
-    
-
-//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal +
-//     "&api_key=uYxydRaxL6nM0E5p2WATKqiFQfi4xXlz&limit=10";
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     })
-//         .then(function(response) {
-
-//             var results = response.data;
-
-//             for (var i = 0; i < results.length; i++) {
-
-//                 var animalSpan = $("<span>");
-
-//                 var p = $("<p>").text("Rating:" + results[i].rating);
-
-//                 var animalImage = $("<img>");
-
-//                 animalImage.attr("src", results[i].images.fixed_height.url);
-
-//                 animalSpan.append(p);
-//                 animalSpan.append(animalImage);
-
-//                 $("#gifs").prepend(animalSpan);
-//             }
-
-//         });
-
-// });
-
-function buildQueryUrl(x) {
-    
-    var animal = $(this).get("data-name");
+    var animal = $(this).attr("dataname");
     console.log(animal);
 
 
@@ -109,6 +74,5 @@ function buildQueryUrl(x) {
 
 }
 
-
-
 console.log(topics);
+
